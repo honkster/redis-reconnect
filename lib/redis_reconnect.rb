2d7779d75d33567b0e2f261cf879fcd8490ca9cd
Brian Takita <brian@honk.com>
2010-09-29 12:16:24 -0700

    Made api more generic by having cache_store_getter passed into initializer.

--- lib/redis_reconnect.rb
+++ lib/redis_reconnect.rb
@@ -1,15 +1,24 @@
-require "cache-store-api"
-
 class RedisReconnect
-  def initialize(app)
+  attr_reader :cache_store_getter
+  def initialize(app, cache_store_getter)
+    require "redis-store"
     @app = app
+    @cache_store_getter = cache_store_getter
   end
 
   def call(env)
-    data = CacheStoreApi.cache.instance_variable_get("@data")
+    data = cache.instance_variable_get("@data")
     if data.respond_to?(:client) && data.client.respond_to?(:on_each_node)
-      CacheStoreApi.cache.instance_variable_get("@data").client.on_each_node :reconnect
+      data.client.on_each_node :reconnect
     end
     @app.call(env)
+  end
+
+  def cache
+    if cache_store_getter.is_a?(Proc)
+      cache_store_getter.call
+    else
+      cache_store_getter
+    end
   end
 end
